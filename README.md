5b89d31a3bd1b4a5818719f37c8fb88921ab788e
Peyoway <100332446+peytwoway@users.noreply.github.com>
2022-05-06 17:48:33 +0000

    Final commit! 😲

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ---
 **A quiz made in Python modeling the Mettaton fight**
 ---
+There are some questions I couldn't put in, as they require pictures. 
+Pretty basic. 
--- quiz.py
+++ quiz.py
@@ -1,6 +1,8 @@
 import time,sys
 import pyfiglet
 import os
+from os import environ
+username = environ["REPL_OWNER"]
 #question("Test", "A", "B", "C", "D", "A"
 def tp(text):
   for character in text:
@@ -17,7 +19,7 @@ def typingInput(text):
 def banner(x):
     banner = banner = pyfiglet.figlet_format(x)
     print(banner)
-def question(title, a, b, c, d, answer):
+def question(title, a, b, c, d, answer, rigged):
   os.system('cls' if os.name == 'nt' else 'clear')
   
   print()
@@ -28,11 +30,28 @@ def question(title, a, b, c, d, answer):
   [C] {c}                  [D] {d}
   ''')
   response = typingInput(" Select an answer! ")
-  time.sleep(2)
-  if answer == response.upper():
-    print("correct!")
+  if rigged:
+    print("Correct!")
     time.sleep(2)
-
+  else: 
+    if answer == response.upper():
+      print("correct!")
+      time.sleep(2)
+    else:
+      death()
+def death():
+  os.system('cls' if os.name == 'nt' else 'clear')
+  banner("GAME OVER")
+  tp("You cannot give up just yet...")
+  time.sleep(2)
+  os.system('cls' if os.name == 'nt' else 'clear')
+  banner("GAME OVER")
+  tp(f"{username}!")
+  print()
+  tp("Stay determined!")
+  print()
+  time.sleep(2)
+  quit()
 def quizmgr():
   os.system('cls' if os.name == 'nt' else 'clear')
   tp("* OH BOY!")
@@ -60,14 +79,49 @@ def quizmgr():
   tp("OR YOU DIE...")
   time.sleep(2)
   os.system('cls' if os.name == 'nt' else 'clear')
-  question("What's the prize for answering correctly?", "Money", "Mercy", "New car", "More questions", "A")
+  question("What's the prize for answering correctly?", "Money", "Mercy", "New car", "More questions", "D", False)
   os.system('cls' if os.name == 'nt' else 'clear')
   tp("RIGHT! SOUNDS LIKE YOU GET IT!")
   time.sleep(2)
   os.system('cls' if os.name == 'nt' else 'clear')
-  question("What's the king's full name?", "Lord Fluffybuns", "Fuzzy Pushover", "Asgore Dreemurr", "Mr. Friendship", "C")
+  question("What's the king's full name?", "Lord Fluffybuns", "Fuzzy Pushover", "Asgore Dreemurr", "Mr. Friendship", "C", False)
   os.system('cls' if os.name == 'nt' else 'clear')
-  question("What are robots made of?", "Hopes & Dreams","Metal & Magic", "Snips & Snails", "Sugar & Spice", "B")
+  question("What are robots made of?", "Hopes & Dreams","Metal & Magic", "Snips & Snails", "Sugar & Spice", "B", False)
+  question("Would you smooch a ghost?", "Heck yeah", "Heck yeah", "Heck yeah", "A", True)
+  question("Two trains, Train A, and Train B, simultaneously depart Station A and Station B. Station A, and Station B are 252.5 miles apart from each other. Train A is moving at 124.7 mph towards Station B, and Train B is moving at 253.5 mph towards Station A. If both trains departed at 10:00 AM and it is now 10:08, how much longer until both trains pass each other?", "31.054 minutes", "16.232 minutes", "32.049 minutes", "32.058 minutes", "C", False)
+  question("How many letters in the name Mettaton?", "11", "6", "8", "10", "C", False)
+  question("Who does Dr. Alphys have a crush on?", "Undyne", "Asgore", "The human", "Don't know", "A", True)
+  tp("WELL WELL WELL")
+  time.sleep(1.5)
+  os.system('cls' if os.name == 'nt' else 'clear') 
+  tp("WITH DR. ALPHYS HELPING YOU...")
+  time.sleep(1.5)
+  tp("THE SHOW HAS NO DRAMATIC TENSION!")
+  time.sleep(1.5)
+  os.system('cls' if os.name == 'nt' else 'clear') 
+  tp("WE CAN'T GO ON LIKE THIS!")
+  time.sleep(1.5)
+  os.system('cls' if os.name == 'nt' else 'clear') 
+  tp("BUT.")
+  time.sleep(1.5)
+  os.system('cls' if os.name == 'nt' else 'clear') 
+  tp("BUT!!")
+  time.sleep(1.5)
+  os.system('cls' if os.name == 'nt' else 'clear') 
+  tp("THIS WAS JUST THE PILOT EPISODE!")
+  time.sleep(1.5)
+  os.system('cls' if os.name == 'nt' else 'clear') 
+  tp("NEXT UP MORE DRAMA!")
+  time.sleep(1.5)
+  os.system('cls' if os.name == 'nt' else 'clear') 
+  tp("MORE ROMANCE!")
+  time.sleep(1.5)
+  os.system('cls' if os.name == 'nt' else 'clear') 
+  tp("MORE BLOODSHED!")
+  time.sleep(1.5)
+  os.system('cls' if os.name == 'nt' else 'clear')
+  tp("UNTIL NEXT TIME DARLINGS!")
+  
   
   
   
